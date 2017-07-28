7710211c92be8414c0458b9fdb42feae40f8157f
Trinh Thanh Phuong <iphuongtt@gmail.com>
2017-07-28 07:35:22 +0700

    Test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
 
-const title = "My Minimal React Webpack Bable Setup";
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
 
 ReactDOM.render(
-  <div>{title}</div>,
+    <Stream tracks={tracks}/>,
   document.getElementById('app')
 );
 module.hot.accept();
